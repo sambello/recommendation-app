859b461b8565b0a43eebb05b5c5ba4b39559f46d
zroga95 <zroga95@gmail.com>
2022-09-06 16:29:56 -0400

    graph updates

--- clustering.py
+++ clustering.py
@@ -93,6 +93,7 @@ def cluster_data(data, n):
     new_df['cluster'] = kmeans.fit_predict(data)
     
     centroids = kmeans.cluster_centers_
+    
     cen_x = [i[0] for i in centroids] 
     cen_y = [i[1] for i in centroids]
     
@@ -107,23 +108,40 @@ def cluster_data(data, n):
     new_df['cen_x'] = new_df.cluster.apply(lambda x:  x_mapper[x])
     new_df['cen_y'] = new_df.cluster.apply(lambda x:  y_mapper[x])
     
-    return new_df
+    return new_df, centroids
 
 
 
-clustered =  pd.concat([numeric_mus, 
-            cluster_data(numeric_mus.drop(columns = ['track_name']), 3)],
-           axis=1)
+clus, centers = cluster_data(numeric_mus.drop(columns = ['track_name']), 3)
+
+clustered =  pd.concat([numeric_mus, clus], axis=1)
 
 colors = ['r', 'g', 'b','y', 'o', 'i']
 clustered['c'] = clustered.cluster.apply(lambda x: colors[x])
 
 
-plt.scatter(clustered.duration_ms, clustered.tempo, c=clustered.c, alpha = 0.6, s=10)
+def plot_axis_cluster(clust, cent, cat1, cat2):
+    divisor = 4
+    
+    plt.scatter(clust[cat1], clust[cat2], c = clust.c, alpha = 0.6, s=10)
+    num1 = clust.columns.get_loc(cat1)
+    num2 = clust.columns.get_loc(cat2)
+    for i in range(0,len(cent)):
+        plt.plot(cent[i][num1], cent[i][num2], 'v', c = 'black')
+        #print(i[num1], i[num2])
+        
+    for i, row in clustered.iterrows():
+        if i % divisor == 0:
+            plt.text(row[cat1], row[cat2], row.track_name)
 
-for i, row in clustered.iterrows():
-    if i%4 == 0:
-        plt.text(row.duration_ms, row.tempo, row.track_name)
-plt.show()
+    plt.xlabel(cat1)
+    plt.ylabel(cat2)
+    
+    return plt
+    
+    
+
+plter = plot_axis_cluster(clustered, centers, 'duration_ms', 'valence')
+plter.show()
 
 
