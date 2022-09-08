fa0f5e2be9a2e2a904c6a1073d97ed7bd96fc906
zroga95 <zroga95@gmail.com>
2022-09-08 17:25:42 -0400

    vector functions

--- clustering.py
+++ clustering.py
@@ -8,6 +8,7 @@ import json
 import pandas as pd
 
 playlist_link = "https://open.spotify.com/playlist/37i9dQZEVXbNG2KDcFcKOF?si=1333723a6eff4b7f"
+playlist_URI = playlist_link.split("/")[-1].split("?")[0]
 
 with open('spotipy_cred.json','r') as f:
     data = json.load(f) 
@@ -18,43 +19,45 @@ client_credentials_manager = SpotifyClientCredentials(client_id=data['SPOTIPY_CL
 sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
 
-playlist_URI = playlist_link.split("/")[-1].split("?")[0]
-track_uris = [x["track"]["uri"] for x in sp.playlist_tracks(playlist_URI)["items"]]
+def get_playlist_info(playlist_id, spot):
+    info_list = []
+    features_list = []
+    
+    for track in spot.playlist_tracks(playlist_id)["items"]:
+         #URI
+        track_uri = track["track"]["uri"]
 
-playlist_list = []
-features_list = []
-for track in sp.playlist_tracks(playlist_URI)["items"]:
-    #URI
-    track_uri = track["track"]["uri"]
-    
-    #Track name
-    track_name = track["track"]["name"]
-    
-    #Main Artist
-    artist_uri = track["track"]["artists"][0]["uri"]
-    artist_info = sp.artist(artist_uri)
-    
-    #Name, popularity, genre
-    artist_name = track["track"]["artists"][0]["name"]
-    artist_pop = artist_info["popularity"]
-    artist_genres = artist_info["genres"]
-    
-    #Album
-    album = track["track"]["album"]["name"]
-    
-    #Popularity of the track
-    track_pop = track["track"]["popularity"]
-    
-    
-    playlist_list.append(
-        [track_uri, track_name, artist_uri, artist_info, artist_name, artist_pop, artist_genres, album, track_pop]
-    )
-    features_list.append(pd.DataFrame(sp.audio_features(track["track"]["uri"])))
+        #Track name
+        track_name = track["track"]["name"]
 
-playlist_df = pd.DataFrame(data = playlist_list, columns = ['track_uri', 'track_name', 'artist_uri',
+        #Main Artist
+        artist_uri = track["track"]["artists"][0]["uri"]
+        artist_info = spot.artist(artist_uri)
+
+        #Name, popularity, genre
+        artist_name = track["track"]["artists"][0]["name"]
+        artist_pop = artist_info["popularity"]
+        artist_genres = artist_info["genres"]
+
+        #Album
+        album = track["track"]["album"]["name"]
+
+        #Popularity of the track
+        track_pop = track["track"]["popularity"]
+
+
+        info_list.append(
+            [track_uri, track_name, artist_uri, artist_info, artist_name, artist_pop, artist_genres, album, track_pop]
+        )
+        features_list.append(pd.DataFrame(spot.audio_features(track["track"]["uri"])))
+        
+        
+    playlist_df = pd.DataFrame(data = info_list, columns = ['track_uri', 'track_name', 'artist_uri',
                                               'artist_info', 'artist_name', 'artist_pop',
                                               'artist_genres', 'album', 'track_pop'])
-full_music = playlist_df.merge(pd.concat(features_list), left_on = 'track_uri', right_on = 'uri')
+    full_music = playlist_df.merge(pd.concat(features_list), left_on = 'track_uri', right_on = 'uri')
+    
+    return full_music
 
 def genre_list_extractor(genre_series):
     genre_set = set()
@@ -66,25 +69,38 @@ def genre_list_extractor(genre_series):
          genre_dict[i] = genre_series.apply(lambda x:  1 if i in x else 0)
     return pd.DataFrame(genre_dict)
 
-categorical_music = pd.concat([genre_list_extractor(full_music.artist_genres), full_music], axis=1)
+def get_sum_vector(machine_df):
+    trackers = machine_df['track_name']
+    vector = machine_df.drop(columns = ['track_name']).sum(axis = 0)
+    return np.divide(vector, machine_df.shape[0])
 
+def get_playlist_subsets(playlist_id, spot):
+    full_music = get_playlist_info(playlist_id, spot)
+    categorical_music = pd.concat([genre_list_extractor(full_music.artist_genres), full_music], axis=1)
+    
+    #TODO scaling
+    typed = categorical_music.dtypes.reset_index()
+    catergories = list(typed[typed[0] != 'object']['index'].unique())
+    catergories.append('track_name')
+    numeric_mus = categorical_music[catergories]
+    
+    vector = get_sum_vector(numeric_mus)
+    
+    return categorical_music, numeric_mus, vector
 
-typed = categorical_music.dtypes.reset_index()
-catergories = list(typed[typed[0] != 'object']['index'].unique())
-catergories.append('track_name')
-numeric_mus = categorical_music[catergories]
+native_fields, machine_fields, vector = get_playlist_subsets(playlist_URI, sp)
 
-
-wcss = []
-for i in range(1,11):
-    kmeans = KMeans(n_clusters=i,init='k-means++',max_iter=300,n_init=10,random_state=0)
-    kmeans.fit(numeric_mus.drop(columns = ['track_name']))
-    wcss.append(kmeans.inertia_)
-plt.plot(range(1,11),wcss)
-plt.title('The Elbow Method')
-plt.xlabel('Number of clusters')
-plt.ylabel('WCSS')
-plt.show()
+def get_cluster_count(df):
+    wcss = []
+    for i in range(1,11):
+        kmeans = KMeans(n_clusters=i,init='k-means++',max_iter=300,n_init=10,random_state=0)
+        kmeans.fit(machine_fields.drop(columns = ['track_name']))
+        wcss.append(kmeans.inertia_)
+    plt.plot(range(1,11),wcss)
+    plt.title('The Elbow Method')
+    plt.xlabel('Number of clusters')
+    plt.ylabel('WCSS')    
+    return plt
 
 
 def cluster_data(data, n):
@@ -110,11 +126,9 @@ def cluster_data(data, n):
     
     return new_df, centroids
 
+clus, centers = cluster_data(machine_fields.drop(columns = ['track_name']), 3)
 
-
-clus, centers = cluster_data(numeric_mus.drop(columns = ['track_name']), 3)
-
-clustered =  pd.concat([numeric_mus, clus], axis=1)
+clustered =  pd.concat([machine_fields, clus], axis=1)
 
 colors = ['r', 'g', 'b','y', 'o', 'i']
 clustered['c'] = clustered.cluster.apply(lambda x: colors[x])
